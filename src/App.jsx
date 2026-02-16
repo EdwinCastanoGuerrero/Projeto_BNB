d701d629a349db9cdc88ebd3d6369fa1173c8d95
EDWIN CASTANO GUERRERO <127364027+EdwinCastanoGuerrero@users.noreply.github.com>
2026-02-16 15:45:05 -0300

    configurando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+function App() {
+  return (
+    <div>
+      <h1>Clone BRB</h1>
+    </div>
+  )
+}
+
+export default App
